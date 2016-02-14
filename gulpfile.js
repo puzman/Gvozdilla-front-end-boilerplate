76328df6e75dcc38bd98738664a6cc8bb2a5cc25
Игорь Смирнов <jenovas@gmail.co>
2016-02-14 22:39:43 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,175 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload,
+    minifyCss = require('gulp-minify-css'),
+    plumber = require('gulp-plumber'),
+    jade = require('gulp-jade'),
+    typograf = require('gulp-typograf'),
+    posthtml = require('gulp-posthtml');
+
+var path = {
+    build: {
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: {
+        jade: 'src/jade/**/*.jade',
+        html: 'src/*.html',
+        js: 'src/js/main.js',
+        style: 'src/style/main.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: {
+        jade: 'src/jade/**/*.jade',
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/style/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './build'
+};
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: false,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Jenovas"
+};
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html)
+        .pipe(plumber())
+        .pipe(rigger())
+    gulp.src(path.src.jade)
+        .pipe(plumber())
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(typograf({
+            lang: 'ru',
+            disable: ['ru/nbsp/initials',
+                'common/symbols/cf']
+
+        }))
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+        .pipe(plumber())
+        .pipe(rigger())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('style:build', function () {
+    gulp.src(path.src.style)
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: ['src/style/'],
+            outputStyle: 'compressed',
+            sourceMap: true,
+            errLogToConsole: true
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(minifyCss())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('fonts:build', function () {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+});
+gulp.task('classes', function () {
+    return gulp.src('build/index.html')
+        .pipe(posthtml([
+            require('posthtml-classes')({
+                fileSave: true,
+                filePath: './src/style/partials/header.scss',
+                overwrite: true,
+                eol: '\n',
+                nested: false
+            })
+
+        ]));
+});
+gulp.task('build', [
+    'html:build',
+    'js:build',
+    'style:build',
+    'fonts:build',
+    'image:build'
+]);
+
+
+gulp.task('watch', function () {
+    watch([path.watch.jade], function (event, cb) {
+        gulp.start('html:build');
+    });
+
+    watch([path.watch.style], function (event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.js], function (event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.img], function (event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function (event, cb) {
+        gulp.start('fonts:build');
+    });
+});
+
+
+gulp.task('default', ['build', 'webserver', 'watch']);
